2b0a35a543de135d8595edecad5c675ce0488a2b
Singh-Rajdiwakar <rajdiwakar5982@gmail.com>
2026-01-31 09:03:40 +0530

    query params and dynamic query

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/user",(req,res)=>{
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params);
     res.send({firstName:"Rajdiwakar", lastName:"Singh"});
 });
 
